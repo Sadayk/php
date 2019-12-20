79f68bd53296f96a997770a4e9a361991da1e5a9
Sadayk <58750612+Sadayk@users.noreply.github.com>
2019-12-20 05:27:27 +0400

    Add files via upload

--- function.php
+++ function.php
@@ -3,21 +3,20 @@ function wordcounter($verse,$filename)
 {
     $verse=str_replace([',','!',';','.','?'],'',$verse);
     $verse=mb_strtolower($verse);
-    print_r($verse);
-//    $fileputstream=fopen($filename,'w');
-//    $array = explode(" ", $verse);
-//    //echo "Всего слов: ", count($array);
-//    fputs($fileputstream, chr(0xEF) . chr(0xBB) . chr(0xBF)); // BOM
-//    fputcsv($fileputstream,array("Всего слов",count($array)),';','"');
-//    foreach ($array as $value) {
-//        if (strlen($value) > 5) {
-//            fputcsv($fileputstream,array($value,mb_substr_count($verse, $value)),';','"');
-//            //echo "Слово '", $value, "' встречается в стихе: ", mb_substr_count($verse, $value), " раз";
-//        } else {
-//            fputcsv($fileputstream,array($value,mb_substr_count($verse, " " . $value . " ")),';','"');
-//            //echo "Слово '", $value, "' встречается в стихе: ", mb_substr_count($verse, " " . $value . " "), " раз";
-//        }
-//    }
+    $fileputstream=fopen($filename,'w');
+    $array = explode(" ", $verse);
+    //echo "Всего слов: ", count($array);
+    fputs($fileputstream, chr(0xEF) . chr(0xBB) . chr(0xBF)); // BOM
+    fputcsv($fileputstream,array("Всего слов",count($array)),';','"');
+    foreach ($array as $value) {
+        if (strlen($value) > 5) {
+            fputcsv($fileputstream,array($value,mb_substr_count($verse, $value)),';','"');
+            //echo "Слово '", $value, "' встречается в стихе: ", mb_substr_count($verse, $value), " раз";
+        } else {
+            fputcsv($fileputstream,array($value,mb_substr_count($verse, " " . $value . " ")),';','"');
+            //echo "Слово '", $value, "' встречается в стихе: ", mb_substr_count($verse, " " . $value . " "), " раз";
+        }
+    }
 }
 if($_POST['text']!=""){
 wordcounter($_POST['text'],'Data1.csv');
